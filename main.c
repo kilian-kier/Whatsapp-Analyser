6a36eb169481a383d0dd43b6bf0035d1bd66d606
mrtngamper <77858688+mrtngamper@users.noreply.github.com>
2021-04-06 21:54:08 +0200

    Some malloc integration

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ typedef struct {
     unsigned int stunde :5;
     unsigned int minute :6;
     char *user;
-    char nachricht[1000];
+    char *nachricht;
 } Nachricht;
 
 int countlines(FILE *f);
@@ -17,40 +17,47 @@ int countlines(FILE *f);
 void print_nachricht(Nachricht *nachricht);
 
 int main(int argc, char *argv[]) {
+    //TODO Stack overflow Problems with large Files
     /*argc - wie viele Paramter (.exe inclusiv)
       argv - Array mit den Paramter (.exe inclusiv)*/
     FILE *f = fopen(argv[1], "r");
     if (f == NULL)
         perror("fopen");
     else {
-        Nachricht arr[countlines(f)];
-        char buffer[1000];
+        Nachricht *arr = (Nachricht *) malloc(countlines(f) * sizeof(Nachricht));
+        char buffer[10000];
         char two_char_buffer[3];
         //strtol convert String to Integer
         char *strtol_buffer[1];
         int i = 0;
-        while (fgets(buffer, 1000, f) != NULL) {
+        while (fgets(buffer, 10000, f) != NULL) {
             strncpy(two_char_buffer, buffer, 2);
-            arr[i].tag = (short)strtol(two_char_buffer, strtol_buffer, 10);
-            strncpy(two_char_buffer, buffer+3, 2);
-            arr[i].monat = (short)strtol(two_char_buffer, strtol_buffer, 10);
-            strncpy(two_char_buffer, buffer+6, 2);
-            arr[i].jahr = (short)strtol(two_char_buffer, strtol_buffer, 10);
-            strncpy(two_char_buffer, buffer+10, 2);
-            arr[i].stunde = (short)strtol(two_char_buffer, strtol_buffer, 10);
-            strncpy(two_char_buffer, buffer+13, 2);
-            arr[i].minute = (short)strtol(two_char_buffer, strtol_buffer, 10);
+            arr[i].tag = (short) strtol(two_char_buffer, strtol_buffer, 10);
+            strncpy(two_char_buffer, buffer + 3, 2);
+            arr[i].monat = (short) strtol(two_char_buffer, strtol_buffer, 10);
+            strncpy(two_char_buffer, buffer + 6, 2);
+            arr[i].jahr = (short) strtol(two_char_buffer, strtol_buffer, 10);
+            strncpy(two_char_buffer, buffer + 10, 2);
+            arr[i].stunde = (short) strtol(two_char_buffer, strtol_buffer, 10);
+            strncpy(two_char_buffer, buffer + 13, 2);
+            arr[i].minute = (short) strtol(two_char_buffer, strtol_buffer, 10);
             //TODO: User und Nachricht kloppn no et gonz
-            if (strchr(buffer+13, ':') != NULL) {
-                arr[i].user = malloc((int)(buffer + 18 - strchr(buffer, ':'))* 3 * sizeof(char));
+            if (strchr(buffer + 13, ':') != NULL) {
+                arr[i].user = malloc((int) (buffer + 18 - strchr(buffer, ':')) * 3 * sizeof(char));
                 strncpy(arr[i].user, buffer + 18, buffer + 18 - strchr(buffer, ':'));
                 arr[i].user[buffer + 18 - strchr(buffer, ':')] = '\0';
             } else
                 arr[i].user = NULL;
-            strcpy(arr[i].nachricht, buffer+18);
+            arr[i].nachricht = (char *) malloc((strlen(buffer + 18) + 1) * sizeof(char));
+            strcpy(arr[i].nachricht, buffer + 18);
             i++;
         }
         print_nachricht(&arr[3]);
+
+        for(int j=0;j<i;j++){
+            free(arr[j].nachricht);
+        }
+        free(arr);
     }
     return 0;
 }
