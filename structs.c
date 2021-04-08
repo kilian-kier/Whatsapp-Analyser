b4ab598e53a0f0caf4d4afca5617e0dea8a9061a
Kier Kilian <kierkilian@gmail.com>
2021-04-08 08:16:58 +0200

    Struct Funktionen

--- structs.c
+++ structs.c
@@ -7,3 +7,24 @@ Nachricht *get_nachricht(int index) {
     }
     return ptr;
 }
+
+void append_nachricht(Nachricht *ptr) {
+    Nachricht *temp = first;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = ptr;
+    ptr->previous = temp;
+    ptr->next = NULL;
+}
+void insert_nachricht(Nachricht *ptr, int index) {
+    Nachricht *temp = get_nachricht(index);
+    Nachricht *next = temp->next;
+    temp->next = ptr;
+    ptr->previous = temp;
+    ptr->next = next;
+}
+void delete_nachricht(Nachricht *ptr) {
+    ptr->previous->next = ptr->next;
+    free(ptr);
+}
--- structs.h
+++ structs.h
@@ -1,6 +1,8 @@
 #ifndef INFORMATIK_PROJEKT_STRUCTS_H
 #define INFORMATIK_PROJEKT_STRUCTS_H
 
+#include "stdlib.h"
+
 typedef struct Nachricht{
     unsigned int tag :5;
     unsigned int monat :4;
@@ -23,7 +25,7 @@ typedef struct User{
 Nachricht *first;
 Nachricht *get_nachricht(int index);
 void append_nachricht(Nachricht *ptr);
-void insert_nachricht(Nachricht *ptr);
+void insert_nachricht(Nachricht *ptr, int index);
 void delete_nachricht(Nachricht *ptr);
 
 #endif //INFORMATIK_PROJEKT_STRUCTS_H
