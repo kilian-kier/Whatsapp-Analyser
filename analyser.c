1845456b24cf401f82bde0682dc8808e10189236
steveplanke <plankestefan@gmail.com>
2021-04-19 19:42:10 +0200

    Nachrichten nach Wochentage zählen
    # Conflicts:
    #	analyser.c
    #	analyser.h

--- analyser.c
+++ analyser.c
@@ -1,30 +1,46 @@
 #include "analyser.h"
 
 void read_user() {
-    Nachricht *nachricht = first_nachricht;
-    User *user = (User *) malloc(sizeof(User));
-    first_user = user;
-    User *temp = user;
-    Nachricht *swap;
-    int n_user = 1;
-    while (nachricht != NULL) {
-        if (nachricht->user == NULL) {
-            nachricht = nachricht->next;
-            continue;
-        }
-        for (int i = 0; i < n_user; i++) {
-            if (strcmp(nachricht->user, temp->name) == 0) {
-                swap = temp->nachrichten->next;
-                temp->nachrichten->next =
-            }
-        }
-        user->name = (char *) malloc(strlen(nachricht->user) * sizeof(char));
-        strcpy(user->name, nachricht->user);
-        user->nachrichten = nachricht;
-        nachricht = nachricht->next;
-        User *next = (User *) malloc(sizeof(User));
-        user->next = next;
-        user = next;
+    Nachricht *ptr = first_nachricht;
+    while (ptr != NULL) {
+
     }
-    printf("%s", first_user->next->name);
 }
+
+
+//Wochentag und Uhrzeit
+//0= Montag
+int *count_date(Nachricht *ptr){
+    int *arr1 = (int *)malloc(7*sizeof(int));
+    for(int x = 0; x < 7; x++){
+        *arr1 = 0;
+        arr1 ++;
+    }
+    short weekday;
+    int h, k;
+    while(ptr->next != NULL){
+        if(ptr->monat <= 2){
+            h = ptr->monat + 12;
+            k = ptr ->jahr -1;
+        }
+        else{
+            h = ptr->monat;
+            k = ptr->jahr;
+        }
+        weekday = (ptr->tag+2*h+(3*h+3)/5+k+k/4-k/100+k/400+1)%7;
+        switch(weekday){
+            case 0: *(arr1+6) += 1; break;
+            case 1: *arr1 += 1; break;
+            case 2: *(arr1+1) += 1; break;
+            case 3: *(arr1+2) += 1; break;
+            case 4: *(arr1+3) += 1; break;
+            case 5: *(arr1+4) += 1; break;
+            case 6: *(arr1+5) += 1; break;
+            default: perror("Fehler Wochentag!"); break;
+        }
+        ptr = ptr->next;
+    }
+    return arr1;
+}
+
+
