931b117eb41c23e52d8d3469481679ca9e8e7ba3
kilian-kier <kierkilian@gmail.com>
2021-06-06 15:10:20 +0200

    wörter suchen

--- include/structs.h
+++ include/structs.h
@@ -27,8 +27,9 @@ typedef struct User {
 
 typedef struct word_list{
     struct word_list *next;
-    char *begin_message;
+    Message *current_message;
     int offset;
+    int number_message;
 } word_list;
 
 typedef struct Dictionary {
@@ -38,6 +39,7 @@ typedef struct Dictionary {
     int amount;
     int length_word;
     int level;
+    int position;
 } Dictionary;
 
 typedef struct Option_tree {
@@ -75,32 +77,51 @@ typedef struct Day_count {
     unsigned int year;
     unsigned int n;
 } Day_count;
-typedef struct Suggestions {
-    char *string;
-    struct Suggestions *next;
-} Suggestions;
 
 typedef struct Settings {
     Color fontcolor;
     Color barcolor;
     Color menucolor;
     Color background;
+    Color highlight_font;
+    Color highlight_back;
     int top_n;
     int empty_lines;
+    int ram_delay;
 } Settings;
 
 union uni {
     int integer;
     char character;
     void *pointer;
+    Dictionary*dict;
 };
 
 typedef struct List {
     struct List *next;
-    union uni item;
+    union uni i;
 } List;
 
+typedef struct Message_list {
+    word_list *message;
+    List *offsets;
+} Message_list;
+
+typedef struct Tree {
+    struct Tree *left;
+    struct Tree *right;
+    struct Tree *parent;
+    word_list *message;
+    List *offsets;
+} Tree;
+
+typedef struct Message_tree {
+    List *words;
+    Tree *messages;
+} Message_tree;
+
 #include "global.h"
+#include "analyser.h"
 
 Option_tree *create_option(wchar_t *opt, void (*function)(), Option_tree *parent, int n_child, int index);
 
@@ -108,5 +129,17 @@ Day_count *create_day_count(unsigned int day, unsigned int month, unsigned int y
 
 List *insert(void *item, List *node, char type);
 List *pop(List *node);
+int get_list_length(List *list);
+char *get_string_from_list(List *list_string);
+List * insert_offset(int *offset, List *node, int word, int max_word);
+Tree *insert_to_tree(word_list *message, Tree *node, Tree *parent, int n_word);
+Tree *get_min_right(Tree *node);
+Tree *get_next_item(Tree *node);
+Tree *get_max_left(Tree *node);
+Tree *get_previous_item(Tree *node);
+Tree *delete_node(Tree *node);
+Tree *update_tree(Tree *node, int words);
+
+void free_list(List *list);
 
 #endif //STRUCTS_H
